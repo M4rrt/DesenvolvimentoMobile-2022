2d08aad7264f09eff723c8dcc4aa9beeae52a68d
Guilherme Martins <67018713+M4rrt@users.noreply.github.com>
2022-11-30 21:22:06 -0300

    Update main.dart

--- coolapp/lib/main.dart
+++ coolapp/lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Oliver's Coffee",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,   
       ),
